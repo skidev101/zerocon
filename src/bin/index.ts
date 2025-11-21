6fa2a350cc67cc665f16f781c53eb8c3218165af
monaski <skidev101@gmail.com>
2025-11-21 23:16:04 +0100

    modified main index file

--- src/bin/index.ts
+++ src/bin/index.ts
@@ -1,36 +1,79 @@
 #!/usr/bin/env node
-
-import { Command } from "commander";
+import {Command} from "commander";
 import prompts from "prompts";
+import fs from "fs-extra";
+import {execa} from "execa"
+import chalk from "chalk"
+
+const program = new Command();
+
+function detectPackageManager() {
+	if (fs.existsSync("pnpm-lock.yaml")) {
+		return "pnpm";
+	}
+	if (fs.existsSync("yarn.lock")) {
+		return "yarn";
+	}
+	if (fs.existsSync("bun.lockb")) {
+		return "bun";
+	}
+	
+	return "npm"
+}
+
+const installCommands = {
+	npm: "npm install",
+	pnpm: "pnpm add",
+	yarn: "yarn add",
+	bun: "bun add"
+}
 
 async function main() {
-	const program = new Command();
-
-program.option("--init [tools...]", "Initilize tools");
-program.parse(process.argv);
-const options = program.opts();
-
-let tools = options.init;
-if (!tools || tools.length === 0) {
-	const response = await prompts({
-		type: "multiselect",
-		name: "tools",
-		message: "Select tools to configure",
-		choices: [
-			{
-				title: "Prisma", value: "prisma"
-			},
-						{
-				title: "Clerk", value: "clerk"
-			},
-						{
-				title: "Auth.js", value: "authjs"
-			},
-		]
-	});
 	
-	tools = response.tools
-}
+	program.name("zerocon").version("1.0.0").addOption(new Command.Option("--init <tools...>", "Initialize tools"));
+
+program.parse(process.argv);
+	
+	let tools = program.opts().init;
+
+	if (typeof tools === 'string') tools = [tools]
+	console.log("tools to initialize:", options)
+	if (!tools || tools.length === 0) {
+		const response = await prompts({
+			type: "multiselect",
+			name: "tools",
+			message: "Select tools to configure",
+			choices: [
+				{
+					title: "Prisma", value: "prisma"
+				},
+							{
+					title: "Clerk", value: "clerk"
+				},
+							{
+					title: "Auth.js", value: "authjs"
+				},
+			]
+		});
+		
+		tools = response.tools || [];
+		console.log("configuring tools:",tools);
+		if (tools.length == 0) {
+			console.log(chalk.yellow("No tools selected. Exiting..."));
+			return;
+		}
+	}
+	
+	const detectedManager = detectPackageManager();
+	try {
+		await execa(installCommands[detectedManager] + ' ' + tools.join(' '), { shell:
+		true})
+		console.log(chalk.green(`Installed ${tools.join(',')} using ${detectedManager}`))
+	} catch (error) {
+		console.error("error installing dependencies:", error)
+		process.exit(1)
+	}
+	
 }
 
 main();
